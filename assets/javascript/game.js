11f34dc2513e170775a5a5573b52a6ab640bebd2
Sam Llamas <sam.llamas@utexas.edu>
2018-04-19 21:36:15 -0500

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,25 +2,30 @@ var guessed = [];
 var words = ["vaporwave", "aesthetic", "lofi", "music"];
 var numTries = 10;
 
+
 var currentWord = words[Math.floor(Math.random() * words.length)].split("");
 var allowed = "abcdefghijklmnopqrstuvwxyz".split("");
-console.log(currentWord);
-
 
 
 
 
 document.onkeyup = function(event) {
      guess = event.key;
+     var tried = false
+     var letter = document.getElementById("letters");
+
 
      
      if (allowed.includes(event.key)){     
 
         if (guessed.indexOf(guess) != -1){
             console.log("You already tried that");
+            tried = true;
         }
         else{
-            guessed.push(guess);  
+            guessed.push(guess);
+            letter.innerHTML += event.key + " ";  
+              
         }
 
         if (currentWord.includes(guess)){
@@ -29,13 +34,13 @@ document.onkeyup = function(event) {
             }
                                                         
         }
-        else{
+        else if (tried === false && numTries != 1){
             numTries --;
+            document.getElementById("hang").setAttribute("src","assets/images/hang"+numTries+".gif");
         }
-
+        
     }  
-  
+   
     
 }
 
-  // for x in the array, if the x is the key that's been press splice it out. 
